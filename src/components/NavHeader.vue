48a8d48ab79f88fe1a149a49bad3da3d33c81616
shijie <2323901052@qq.com>
2020-03-17 07:45:59 +0800

    完成头部菜单区样式开发

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -15,7 +15,33 @@
                 </div>
             </div>
         </div>
-        <div class="nav-header"></div>
+        <div class="nav-header">
+            <div class="container">
+                <div class="header-logo">
+                    <a href="/#/index"></a>
+                </div>
+                <div class="header-menu">
+                    <div class="item-menu">
+                        <span>小米手机</span>
+                        <div class="children"></div>
+                    </div>
+                    <div class="item-menu">
+                        <span>RedMi红米</span>
+                        <div class="children"></div>
+                    </div>
+                    <div class="item-menu">
+                        <span>电视</span>
+                        <div class="children"></div>
+                    </div>
+                </div>
+                <div class="header-search">
+                    <div class="wrapper">
+                        <input type="text" name="keyword">
+                        <a href="javascript:;"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -25,6 +51,8 @@ export default {
 }
 </script>
 <style lang="scss">
+@import './../assets/scss/base.scss';
+@import './../assets/scss/mixin.scss';
 .header {
     .nav-topbar {
         height: 39px;
@@ -32,12 +60,7 @@ export default {
         background-color: #333333;
         color: #B0B0B0;
         .container {
-            width: 1226px;
-            margin-right: auto;
-            margin-left: auto;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
+            @include flex();
             a {
                 display: inline-block;
                 color: #B0B0B0;
@@ -49,14 +72,80 @@ export default {
                 text-align: center;
                 color: #ffffff;
                 .icon-cart {
-                    display: inline-block;
-                    width: 16px;
-                    height: 12px;
+                    @include bgImg(16px, 12px, '/imgs/icon-cart-checked.png', contain);
                     margin-right: 4px;
-                    background: url('/imgs/icon-cart-checked.png') no-repeat center;
-                    background-size: contain;
                 }
             }
+        }
+    }
+    .nav-header {
+        .container {
+            height: 112px;
+            @include flex();
+            .header-logo {
+                display: inline-block;
+                width: 55px;
+                height: 55px;
+                background-color: #FF6600;
+                a {
+                    display: inline-block;
+                    width: 110px;
+                    height: 55px;
+                    &:before {
+                        content: ' ';
+                        @include bgImg(55px, 55px, '/imgs/mi-logo.png', 55px);
+                        transition:margin .2s;
+                    }
+                    &:after {
+                        content: ' ';
+                        @include bgImg(55px, 55px, '/imgs/mi-home.png', 55px);
+                    }
+                    &:hover:before  {
+                        margin-left: -55px;
+                        transition:margin .2s;
+                    }
+                }
+            }
+            .header-menu {
+                display: inline-block;
+                // width: 200px;
+                padding-left: 209px;
+                .item-menu {
+                   display: inline-block;
+                   color: #333333;
+                   font-weight: bold; 
+                   font-size: 16px;
+                   line-height: 112px;
+                   margin-right: 20px;
+                   span {
+                       cursor: pointer;
+                   }
+                   &:hover {
+
+                   }
+                }
+            }
+            .header-search {
+                    width: 319px;
+                    .wrapper {
+                        height: 50px;
+                        border: 1px solid #E0E0E0;
+                        display: flex;
+                        align-items: center;
+                        input {
+                            border: none;
+                            box-sizing: border-box;
+                            border-right: 1px solid #E0E0E0;
+                            width: 264px;
+                            height: 50px;
+                            padding-left: 14px;
+                        }
+                        a {
+                            @include bgImg(18px, 18px, '/imgs/icon-search.png', contain);
+                            margin-left: 17px;
+                        }
+                    }
+                }
         }
     }
 }
